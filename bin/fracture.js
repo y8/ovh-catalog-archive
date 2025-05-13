77cf379a9db84d06d640cbdfebb08474d8b7bf47
y8 <y8@users.noreply.github.com>
2025-05-13 20:56:34 +0200

    FracturedJson processor now accepts data from `stdin`

--- bin/fracture.js
+++ bin/fracture.js
@@ -1,10 +1,34 @@
 const { Formatter, FracturedJsonOptions } = require("fracturedjsonjs");
-
 const fs = require("fs");
 
-// read file path from argv
-const filePath = process.argv[2];
-const inputText = JSON.parse(fs.readFileSync(filePath, "utf8"));
+async function getJSON() {
+  const filePath = process.argv[2];
+
+  if (filePath) {
+    return JSON.parse(fs.readFileSync(filePath, "utf8"));
+  } else {
+    // read from stdin
+    return new Promise((resolve, reject) => {
+      let data = "";
+
+      process.stdin.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      process.stdin.on("end", () => {
+        try {
+          resolve(JSON.parse(data));
+        } catch (error) {
+          reject(`Error parsing JSON: ${error.message}`);
+        }
+      });
+
+      process.stdin.on("error", (error) => {
+        reject(`Error reading from stdin: ${error.message}`);
+      });
+    });
+  }
+}
 
 const options = new FracturedJsonOptions();
 
@@ -24,8 +48,20 @@ options.CommaPadding = true;
 
 options.OmitTrailingWhitespace = true;
 
-const formatter = new Formatter();
-formatter.Options = options;
-const formattedText = formatter.Serialize(inputText);
+async function main() {
+  try {
+    const catalog = await getJSON();
 
-console.log(formattedText);
+    const formatter = new Formatter();
+    formatter.Options = options;
+
+    const fracturedCatalog = formatter.Serialize(catalog);
+
+    console.log(fracturedCatalog);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+main();
